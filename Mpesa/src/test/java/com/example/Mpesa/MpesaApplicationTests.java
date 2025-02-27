2ec27df2b6c7712b3ceab4c8ca2f19d4b69c005c
Sheryl Chepngetich <106540711+torongeis@users.noreply.github.com>
2025-02-27 08:11:37 +0300

    mpesa files

---
+++ Mpesa/src/test/java/com/example/Mpesa/MpesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mpesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
